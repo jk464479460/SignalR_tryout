76a9c07dfa5c9f608ffe80a00042fc79474f6825
jhon <464479460@qq.com>
2017-05-30 10:32:52 +0800

    添加项目文件。

---
+++ WebApplication3/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace WebApplication3
+{
+    [HubName("chat")]
+    public class ChatHub:Hub
+    {
+        public void Send(string name , string message )
+        {
+            Clients.All.broadcastMessage(name, message);
+        }
+
+       
+    }
+}
---
+++ WebApplication3/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebApplication3.Startup))]
+namespace WebApplication3
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //Anyconnectionorhubwireupandconfigurationshouldgohere
+            app.MapSignalR();
+            #region WebApi
+            var httpConfig = new HttpConfiguration();
+            httpConfig.Routes.MapHttpRoute(
+                    name: "DefaultApi",
+                    routeTemplate: "api/{controller}/{action}/{id}",
+                    defaults: new { id = RouteParameter.Optional }
+                );
+            //强制设定目前的WebApi返回格式为json
+            httpConfig.Formatters.Remove(httpConfig.Formatters.XmlFormatter);
+            //加载WebApi中间件
+            app.UseWebApi(httpConfig);
+            #endregion
+        }
+    }
+
+    public class Startup2
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //Anyconnectionorhubwireupandconfigurationshouldgohere
+            #region WebApi
+            var httpConfig = new HttpConfiguration();
+            httpConfig.Routes.MapHttpRoute(
+                    name: "DefaultApi",
+                    routeTemplate: "api/{controller}/{action}/{id}",
+                    defaults: new { id = RouteParameter.Optional }
+                );
+            //强制设定目前的WebApi返回格式为json
+            httpConfig.Formatters.Remove(httpConfig.Formatters.XmlFormatter);
+            //加载WebApi中间件
+            app.UseWebApi(httpConfig);
+            #endregion
+        }
+    }
+}
